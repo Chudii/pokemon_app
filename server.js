dac33f5f8fdbcb9bbbe21adbb030e40d839ea3d9
Chudi <chudiibida3@gmail.com>
2022-07-28 13:23:21 -0400

    All my files are created

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3003
+
+
+
+app.listen(port, () => {
+    console.log(`*** Listening on http://localhost:${port} ***`)
+})
